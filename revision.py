9b67f10516debaffe1271ef781e4a0c22341f4d7
Zeshan Ali <zishraj07@gmail.com>
2023-08-04 12:55:11 +0500

    updating files

--- revision.py
+++ revision.py
@@ -1,1 +1,18 @@
 print("i am using gothub now")
+
+percentage = float(input("Please enter your percentage"))
+
+if(percentage > 80):
+    print("grade is A")
+elif (percentage > 70):
+    print("grade is b")
+elif (percentage > 60):
+    print("grade is c")
+elif (percentage > 50):
+    print("grade is d")
+elif (percentage > 40):
+    print("grade is e")
+elif (percentage > 30):
+    print("grade is f")
+else:
+    print("nalaiq sahee value enter kro")
